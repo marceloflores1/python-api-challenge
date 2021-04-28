b8e5bd00daa6af27d900e5b19230684bd3f48bb3
Marcelo Flores <79871752+marceloflores1@users.noreply.github.com>
2021-04-28 01:28:45 -0500

    Finishing scatter plots

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e70a466f5708b545582ce9015e9ee181"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAUeDoNkPHffPLdU3sQb648kxjkoZv4CVs"
